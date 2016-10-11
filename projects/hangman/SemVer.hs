12599a8ea6ea60dabc88cfe64f29c15ec63d5c49
Tom Smalley <tfsmalley@gmail.com>
2016-10-11 08:39:19 +0100

    Finished semantic versioning parser exercise

--- projects/hangman/SemVer.hs
+++ projects/hangman/SemVer.hs
@@ -1,6 +1,9 @@
 import Control.Applicative
 import Text.Trifecta
 import Data.List (intercalate, sort)
+import Test.Hspec
+import Data.Foldable (toList)
+import Text.Read (readMaybe)
 
 data NumberOrString = NOSS String
                     | NOSI Integer
@@ -36,42 +39,66 @@ instance Ord SemVer where
                          | otherwise = compare p p'
 
 parseSemVer :: Parser SemVer
-parseSemVer = undefined
+parseSemVer = do
+    major <- integer
+    char '.'
+    minor <- integer
+    char '.'
+    patch <- integer
+    release <- option [] $ char '-' >> sepBy1 parseNos (char '.')
+    metadata <- option [] $ char '+' >> sepBy1 parseNos (char '.')
+    return $ SemVer major minor patch release metadata
 
 parseNos :: Parser NumberOrString
 parseNos = do
-    --skipMany (oneOf "\n")
-    v <- (NOSI <$> integer) <|> (NOSS <$> some letter)
-    --skipMany (oneOf "\n")
-    return v
+    chr <- some alphaNum
+    let ints = readMaybe chr :: Maybe Integer
+    case ints of
+         Just i -> return $ NOSI i
+         Nothing -> return $ NOSS chr
 
-main = do
-    print "Show testing"
-    let testmeta = [ (SemVer 1 0 0 [NOSS "alpha"] [NOSS "001"])
-                   , (SemVer 1 0 0 [] [NOSI 20130313144700])
-                   , (SemVer 1 0 0 [NOSS "beta"] [NOSS "exp", NOSS "sha", NOSS "5114f85"])
-                   ]
-    print testmeta
+show1 = SemVer 1 0 0 [NOSS "alpha"] [NOSS "001"]
+show2 = SemVer 1 0 0 [] [NOSI 20130313144700]
+show3 = SemVer 1 0 0 [NOSS "beta"] [NOSS "exp", NOSS "sha", NOSS "5114f85"]
+testorder = [ (SemVer 1 0 0 [NOSS "alpha"] [])
+            , (SemVer 1 0 0 [NOSS "alpha", NOSI 1] [])
+            , (SemVer 1 0 0 [NOSS "alpha", NOSS "beta"] [])
+            , (SemVer 1 0 0 [NOSS "beta"] [])
+            , (SemVer 1 0 0 [NOSS "beta", NOSI 2] [])
+            , (SemVer 1 0 0 [NOSS "beta", NOSI 11] [])
+            , (SemVer 1 0 0 [NOSS "rc", NOSI 1] [])
+            , (SemVer 1 0 0 [] [])
+            , (SemVer 1 0 1 [] [])
+            , (SemVer 1 1 1 [] [])
+            , (SemVer 2 0 0 [] [])
+            ]
 
-    print "Ord testing - book example"
-    print $ SemVer 2 1 1 [] [] > SemVer 2 1 0 [] []
-    print "Ord testing - semver.org example"
-    let testorder = [ (SemVer 1 0 0 [NOSS "alpha"] [])
-                    , (SemVer 1 0 0 [NOSS "alpha", NOSI 1] [])
-                    , (SemVer 1 0 0 [NOSS "alpha", NOSS "beta"] [])
-                    , (SemVer 1 0 0 [NOSS "beta"] [])
-                    , (SemVer 1 0 0 [NOSS "beta", NOSI 2] [])
-                    , (SemVer 1 0 0 [NOSS "beta", NOSI 11] [])
-                    , (SemVer 1 0 0 [NOSS "rc", NOSI 1] [])
-                    , (SemVer 1 0 0 [] [])
-                    , (SemVer 1 0 1 [] [])
-                    , (SemVer 1 1 1 [] [])
-                    , (SemVer 2 0 0 [] [])
-                    ]
-    print testorder
-    print . sort $ reverse testorder
-    print $ testorder == sort testorder
+ex1 = parseString parseSemVer mempty "2.1.1"
+an1 = SemVer 2 1 1 [] []
+ex2 = parseString parseSemVer mempty "1.0.0-x.7.z.92"
+an2 = SemVer 1 0 0 [NOSS "x", NOSI 7, NOSS "z", NOSI 92] []
+ex3 = parseString parseSemVer mempty "0.0.1-123a.456+test"
+an3 = SemVer 0 0 1 [NOSS "123a", NOSI 456] [NOSS "test"]
 
-    print "Parse testing"
-    print $ parseString parseSemVer mempty "2.1.1"
-    print $ parseString parseSemVer mempty "1.0.0-x.7.z.92"
+main = hspec $ do
+    describe "Show testing" $ do
+        it "Prints 1.0.0-alpha+001 correctly" $ do
+            show show1 `shouldBe` "1.0.0-alpha+001"
+        it "Prints 1.0.0+20130313144700 correctly" $ do
+            show show2 `shouldBe` "1.0.0+20130313144700"
+        it "Prints 1.0.0-beta+exp.sha.5114f85 correctly" $ do
+            show show3 `shouldBe` "1.0.0-beta+exp.sha.5114f85"
+    describe "Ord testing - book example" $ do
+        it "2.1.1 is greater than 2.1.0" $ do
+            SemVer 2 1 1 [] [] > SemVer 2 1 0 [] [] `shouldBe` True
+    describe "Ord testing - semver.org example" $ do
+        it "Should be the correct order" $ do
+            testorder `shouldBe` sort (reverse testorder)
+    describe "Parse testing - book example" $ do
+        it "Parses 2.1.1 correctly" $ do
+            toList ex1 `shouldBe` [an1]
+        it "Parses 1.0.0-x.7.z.92 correctly" $ do
+            toList ex2 `shouldBe` [an2]
+    describe "Parse testing - my example" $ do
+        it "Parses 0.0.1-123a.456+test correctly" $ do
+            toList ex3 `shouldBe` [an3]
